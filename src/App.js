720d17923da50a36c254209983db25b6a6d63a9b
sabbirahammadProf <sabbirahammad212@gmail.com>
2021-09-30 23:31:07 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
